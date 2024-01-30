dc5fbcd90ecd21810aa4c915f9357d597e29a009
Tamir Puzanov <58332679+TamirPuzanov@users.noreply.github.com>
2024-01-30 19:09:26 +0700

    Add ResCNN model

--- srcs/model/model.py
+++ srcs/model/model.py
@@ -62,3 +62,95 @@ class Conv3(nn.Module):
         x = F.log_softmax(self.dense3(x), dim=1)
 
         return x
+
+
+# ResCNN (Large 231M, SMALL 18M)
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+
+        self.layer = nn.Sequential(
+            nn.Conv2d(in_channels, in_channels, 3, 1, 1),
+            nn.LeakyReLU(), nn.BatchNorm2d(in_channels),
+
+            nn.Conv2d(in_channels, in_channels, 3, 1, 1),
+            nn.LeakyReLU(), nn.BatchNorm2d(in_channels)
+        )
+    
+    def forward(self, x):
+        return self.layer(x) + x
+
+
+class CNN_Large(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 256, 3, 1, 1), 
+            nn.LeakyReLU(), nn.BatchNorm2d(256),
+
+            nn.Sequential(*[
+                ResidualBlock(256) for _ in range(3)
+            ]),
+
+            nn.Conv2d(256, 512, 5, 2, 2),
+            nn.LeakyReLU(), nn.BatchNorm2d(512),
+
+            nn.Sequential(*[
+                ResidualBlock(512) for _ in range(4)
+            ]),
+
+            nn.Conv2d(512, 1024, 3, 2, 1),
+            nn.LeakyReLU(), nn.BatchNorm2d(1024),
+
+            nn.Sequential(*[
+                ResidualBlock(1024) for _ in range(6)
+            ]),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+
+            nn.Linear(1024, num_classes)
+        )
+    
+    def forward(self, x):
+        x = F.log_softmax(self.main(x), dim=1)
+        return x
+
+
+class CNN_Small(nn.Module):
+    def __init__(self, num_classes=10):
+        super().__init__()
+
+        self.main = nn.Sequential(
+            nn.Conv2d(1, 128, 5, 2, 1), 
+            nn.LeakyReLU(), nn.BatchNorm2d(128),
+
+            nn.Sequential(*[
+                ResidualBlock(128) for _ in range(2)
+            ]),
+
+            nn.Conv2d(128, 256, 3, 2, 1),
+            nn.LeakyReLU(), nn.BatchNorm2d(256),
+
+            nn.Sequential(*[
+                ResidualBlock(256) for _ in range(2)
+            ]),
+
+            nn.Conv2d(256, 512, 3, 2, 1),
+            nn.LeakyReLU(), nn.BatchNorm2d(512),
+
+            nn.Sequential(*[
+                ResidualBlock(512) for _ in range(3)
+            ]),
+
+            nn.AdaptiveAvgPool2d(1),
+            nn.Flatten(),
+
+            nn.Linear(512, num_classes)
+        )
+    
+    def forward(self, x):
+        x = F.log_softmax(self.main(x), dim=1)
+        return x
